2fa56c8592cd53a63c0f9ee1c6dd7d381110c2a6
SpaceNeuro <totoshkus@gmail.com>
2024-07-31 09:00:20 +0200

    ServerVevsion

--- ruSpamLib/detector.py
+++ ruSpamLib/detector.py
@@ -1,28 +1,29 @@
-import torch
-import re
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import requests
 
-model_name = 'NeuroSpaceX/ruSpam_V1'
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+def is_spam(message, key="", model_type='default'):
+    api_url = "https://neurospacex-keysapiserv.hf.space/check_spam"
+    
+    payload = {
+        "message": message,
+        "api_key": key,
+        "model_type": model_type
+    }
+    
+    response = requests.post(api_url, json=payload)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-model.eval()
+    if response.status_code == 200:
+        result = response.json()
+        return result['is_spam'] == 1
+    elif response.status_code == 403:
+        result = response.json()
+        if 'detail' in result and result['detail'] == 'Invalid API key':
+            print("Неверный API ключ.")
+        else:
+            print("Ошибка при запросе к API:", result.get('detail', 'Неизвестная ошибка'))
+    else:
+        print("Ошибка при запросе к API:", response.status_code)
+    
+    return False
+    
 
-def clean_text(text):
-    text = re.sub(r'http\S+', '', text)  
-    text = re.sub(r'[^А-Яа-я0-9 ]+', ' ', text)
-    text = text.lower().strip()
-    return text
-
-def is_spam(message):
-    message = clean_text(message)
-    encoding = tokenizer(message, padding='max_length', truncation=True, max_length=128, return_tensors='pt')
-    input_ids = encoding['input_ids'].to(device)
-    attention_mask = encoding['attention_mask'].to(device)
-
-    with torch.no_grad():
-        outputs = model(input_ids, attention_mask=attention_mask).logits
-        pred = torch.sigmoid(outputs).cpu().numpy()[0][0]
-    return pred >= 0.5
+    
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ruSpam',
-    version='0.1.0',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'torch',
